5ac1edff3328385ecb3e7c8864f211587c0d6696
Markus Tacker <m@coderbyheart.com>
2025-02-12 16:17:05 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# LwM2M Gateway
+
+This is a sample client that simulates a gateway device which publishes LwM2M
+objects on behalf of other devices.
+
+The supported LwM2M objects are defined in
+<https://github.com/hello-nrfcloud/proto-map>.
+
+
---
+++ publish.ts
@@ -0,0 +1,58 @@
+import mqtt from "mqtt";
+import cert from "./certificate.json" assert { type: "json" };
+import { readFileSync } from "node:fs";
+import path from "node:path";
+import { lwm2mToSenML } from "@hello.nrfcloud.com/proto-map/senml";
+import {
+  type Environment_14205,
+  LwM2MObjectID,
+} from "@hello.nrfcloud.com/proto-map/lwm2m";
+
+const temp: Environment_14205 = {
+  ObjectID: LwM2MObjectID.Environment_14205,
+  ObjectVersion: "1.0",
+  Resources: {
+    "0": 21.7, // Temperature
+    "99": Math.floor(Date.now() / 1000), // Unix time in seconds
+  },
+};
+const maybeSenML = lwm2mToSenML(temp);
+
+if ("errors" in maybeSenML) {
+  throw new Error(
+    "Failed to convert LwM2M to SenML: " + JSON.stringify(maybeSenML.errors)
+  );
+}
+
+const { clientId, certificate, privateKey } = cert;
+
+const mqttClient = mqtt.connect({
+  host: "iot.thingy.rocks",
+  port: 8883,
+  protocol: "mqtts",
+  protocolVersion: 4,
+  clean: true,
+  clientId,
+  key: privateKey,
+  cert: certificate,
+  ca: readFileSync(path.join(process.cwd(), "AmazonRootCA1.pem"), "utf-8"),
+});
+
+mqttClient.on("connect", () => {
+  console.log("connected");
+
+  const payload = JSON.stringify(maybeSenML.senML);
+  const topic = `${clientId}/lwm2m-gateway/senml/ntn1`;
+
+  mqttClient.publish(topic, payload, { qos: 1 }, (err) => {
+    if (err) {
+      throw new Error("Failed to publish: " + err);
+    }
+    console.log("published", topic, payload);
+    mqttClient.end();
+  });
+});
+
+mqttClient.on("error", (err) => {
+  console.error(err);
+});
